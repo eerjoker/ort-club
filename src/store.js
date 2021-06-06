351fcc669d6e3465813b810843bba03bc96d26fe
eerjoker <rivera.esteban1994@gmail.com>
2021-06-06 19:41:41 -0300

    obtengo usuarios por peticion axios

--- src/store.js
+++ src/store.js
@@ -1,19 +1,14 @@
 import Vue from "vue";
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
+    url: 'https://60bd1820b8ab3700175a020d.mockapi.io/api/',
     usuarioActual: null,
-    usuarios: [
-      {
-        id: 100,
-        nombre: 'Esteban',
-        mail: '1@mail.com',
-        password: '123'
-      }
-    ]
+    usuarios: []
   },
   getters: {
     nombreUsuarioActual: (state) => {
@@ -24,13 +19,16 @@ const store = new Vuex.Store({
       }
     },
     usuarioLogin: (state) => (email, password) => {
-      return state.usuarios.find(u => u.mail == email && u.password == password)
+      return state.usuarios.find(u => u.email == email && u.password == password)
     },
     hayUsuario: (state) => {
       return state.usuarioActual != null
     }
   },
   mutations: {
+    setUsuarios (state, usuarios) {
+      state.usuarios = usuarios
+    },
     login (state, payload) {
       state.usuarioActual = payload.usuario
     },
@@ -39,8 +37,19 @@ const store = new Vuex.Store({
     }
   },
   actions: {
+    async setUsuarios (context) {
+      try {
+        const usuariosResponse = await axios.get(`${ context.state.url }/usuarios`)
+        if(usuariosResponse.status != 200) {
+          throw new Error('Usuarios: ' + usuariosResponse.statusText)
+        }
+        context.commit('setUsuarios', usuariosResponse.data)
+      } catch(err) {
+        alert(err.message)
+      }
+    },
     loguear (context, usuario) {
-      context.commit('login', usuario)
+      context.commit('login', { usuario })
     },
     desloguear (context) {
       context.commit('logout')
