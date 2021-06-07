bab57388f70e39585cd7fc05fa5d5967b0c9f968
Agustin Malvaso <agustin.malvaso@gmail.com>
2021-06-07 19:59:07 -0300

    Vista de ListaUsuarios creada

---
+++ src/components/ListaUsuarios.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+      <li v-for="(actividad, index) in $store.getters.getActividadesPorTipo(tipo)" :key="index">
+         {{actividad.nombre}} 
+      </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    
+  },
+  computed: {
+    
+  },
+};
+</script>
+
+<style>
+
+</style>
