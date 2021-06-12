eea13cd3f9f06b9faa4e83f55050c70c341a009b
ezesalo <ezesalo@gmail.com>
2021-06-12 18:42:51 -0300

    Agregada la vista de Actividad

--- src/components/Actividad.vue
+++ src/components/Actividad.vue
@@ -1,14 +1,41 @@
 <template>
   <div>
-    <p>Soy el actividad actual</p>
-    {{ $route.params.id }}  
-   
+    <b-img :src= "this.actividad.imagen" fluid alt="Fluid image"></b-img>
+    <h1>{{this.actividad.nombre}}</h1>
+    <h3>{{this.actividad.descripcion}}</h3>
   </div>
 </template>
 
 <script>
-
+import axios from 'axios';
 export default {
-name: "Actividad"
+name: "Actividad",
+data:()=>({
+    idActividad: -1,
+    actividad: {}
+  }),
+methods:{
+  async getActividad(){
+    try {
+      const actividadResponse = await axios.get(`${ this.$store.state.url }/actividades/${this.$route.params.id}`)
+      this.actividad = actividadResponse.data
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+},
+async created() {
+    this.idActividad = this.$route.params.id
+    await this.getActividad()
+    this.$watch(
+      () => this.$route.params,
+      toParams => {
+        // react to route changes...
+        this.actividad = {}
+        this.idActividad = toParams.id
+        this.getActividad()
+      }
+    )
+  }
 }
 </script>
