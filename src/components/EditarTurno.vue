c00debdcaf925a8b73cfd0b3b26ce73ace1c56e3
ezesalo <ezesalo@gmail.com>
2021-06-26 18:42:08 -0300

    ajuste en Editar Turno

--- src/components/EditarTurno.vue
+++ src/components/EditarTurno.vue
@@ -1,65 +1,115 @@
 <template>
-  <div>
-    <b-form-input style="width: 50%"
-      id="input-1"
-      v-model="turno.tituloTurno"
-      type="text"
-      placeholder="titulo del turno"
-    ></b-form-input>
-    <br>
-    <b-form-input style="width: 50%"
-      id="input-2"
-      v-model="turno.idProfesor"
-      type="text"
-      placeholder="nombre del profesor"
-    ></b-form-input>
-    <br>
-    <b-form-input style="width: 50%"
-      id="input-3"
-      v-model="turno.fecha"
-      type="date"
-      placeholder="fecha del turno"
-    ></b-form-input>
-    <b-form-input style="width: 50%"
-      id="input-4"
-      v-model="turno.idActividad"
-      type="number"
-      placeholder="ID de la actividad"
-    ></b-form-input>
-    <button class="m-1" @click="modificarTurno()"> Modificar Turno </button>
+  <div class="w-40 mx-auto">
+    <b-form>
+      <b-container fluid>
+        <b-form-group
+          id="group-tituloTurno"
+          class="justify-content-between align-middle"
+          label="Titulo del turno:"
+          label-for="titulo"
+        >
+          <b-form-input
+            id="titulo"
+            v-model="turno.tituloTurno"
+            type="text"
+            placeholder="titulo del turno"
+            required
+          >
+          </b-form-input>
+        </b-form-group>
+
+        <br />
+
+        <b-form-group
+          id="group-profesor"
+          class="justify-content-between align-middle"
+          label="Nombre del profesor:"
+          label-for="profesor"
+        >
+          <b-form-input
+            id="profesor"
+            v-model="turno.idProfesor"
+            type="text"
+            placeholder="nombre del profesor"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <br />
+
+        <b-form-group
+          id="group-fecha"
+          class="justify-content-between align-middle"
+          label="Fecha del turno:"
+          label-for="fecha"
+        >
+          <b-form-input
+            id="fecha"
+            v-model="turno.fecha"
+            type="date"
+            placeholder="fecha del turno"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <br />
+
+        <b-form-group
+          id="group-actividad"
+          class="justify-content-between align-middle"
+          label="ID de la actividad:"
+          label-for="actividad"
+        >
+          <b-form-input
+            id="actividad"
+            v-model="turno.idActividad"
+            type="number"
+            placeholder="id de la actividad"
+            required
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button class="m-1" @click="modificarTurno()">Modificar Turno</b-button>
+      </b-container>
+    </b-form>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   data: () => ({
     turnoId: -1,
-    turno: {}
+    turno: {},
   }),
   methods: {
-    async modificarTurno(){
-      console.log(this.turno)
-      await axios.put(`${ this.$store.state.urlTurnos }/turnos/${this.turnoId}`, this.turno)
-      this.$router.push('/listaTurnos')
+    async modificarTurno() {
+      console.log(this.turno);
+      await axios.put(
+        `${this.$store.state.urlTurnos}/turnos/${this.turnoId}`,
+        this.turno
+      );
+      this.$router.push("/listaTurnos");
+    },
+  },
+  async created() {
+    this.turnoId = this.$route.params.id;
+    try {
+      const turnosResponse = await axios.get(
+        `${this.$store.state.urlTurnos}/turnos/${this.turnoId}`
+      );
+      if (turnosResponse.status < 200 || turnosResponse.status >= 300) {
+        throw new Error(
+          "Error al cargar los turnos: " + turnosResponse.statusText
+        );
+      }
+      this.turno = turnosResponse.data;
+    } catch (err) {
+      alert(err.message);
     }
   },
-  async created(){
-    this.turnoId = this.$route.params.id
-    try {
-      const turnosResponse = await axios.get(`${ this.$store.state.urlTurnos }/turnos/${this.turnoId}`)
-      if(turnosResponse.status < 200 || turnosResponse.status >= 300) {
-        throw new Error('Error al cargar los turnos: ' + turnosResponse.statusText)
-      }
-      this.turno = turnosResponse.data
-    } catch(err) {
-      alert(err.message)
-    }
-  }
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
