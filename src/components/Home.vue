6ccf707b4b56072cc3b7f6be0543146e98f41e24
Agustin Malvaso <agustin.malvaso@gmail.com>
2021-06-26 16:29:12 -0300

    Metodo para filtrar reservas agregado

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <div v-if="hayUsuario">
-      <Calendar />
+      <Calendar :usuarioId="usuarioActualId" :usuarioTipo="usuarioActualTipo" />
     </div>
+
     <div class="w-30 p-3 mx-auto" v-if="!hayUsuario">
       <Registro />
     </div>
@@ -12,17 +13,97 @@
 <script>
 import Calendar from "./Calendar.vue";
 import Registro from "./Registro.vue";
+import axios from "axios";
 
 export default {
+  data() {
+    return {
+      reservas: [],
+      actividades: [],
+      reservasFinal: [],
+    };
+  },
   name: "Home",
   components: {
     Calendar,
-    Registro
+    Registro,
   },
   computed: {
     hayUsuario() {
-      return this.$store.getters.hayUsuario
-    }
-  }
-}
+      return this.$store.getters.hayUsuario;
+    },
+    usuarioActualId() {
+      return this.$store.getters.usuarioActualId;
+    },
+    usuarioActualTipo() {
+      return this.$store.getters.usuarioActualTipo;
+    },
+  },
+  async created() {
+    await this.getReservas();
+    await this.getActividades();
+    this.setReservaFinal();
+    console.log(this.reservasFinal);
+  },
+  methods: {
+    async getReservas() {
+      try {
+        const reservasResponse = await axios.get(
+          `${this.$store.state.url}/reservas`
+        );
+        if (reservasResponse.status < 200 || reservasResponse.status >= 300) {
+          throw new Error(
+            "Error al cargar los usuarios: " + reservasResponse.statusText
+          );
+        }
+        this.reservas = this.reservasFiltradas(reservasResponse.data);
+      } catch (err) {
+        alert(err.message);
+      }
+    },
+    async getActividades() {
+      try {
+        const actividadesResponse = await axios.get(
+          `${this.$store.state.url}/actividades`
+        );
+        if (
+          actividadesResponse.status < 200 ||
+          actividadesResponse.status >= 300
+        ) {
+          throw new Error(
+            "Error al cargar los usuarios: " + actividadesResponse.statusText
+          );
+        }
+        this.actividades = actividadesResponse.data;
+      } catch (err) {
+        alert(err.message);
+      }
+    },
+    reservasFiltradas(reservas) {
+      if (this.usuarioActualTipo != "socio") {
+        return reservas;
+      } else {
+        return reservas.filter((r) => r.idUsuario == this.usuarioActualId);
+      }
+    },
+    getNombreActividad(idActividad) {
+      let actividad = this.actividades.find(
+        (act) => parseInt(act.id) == idActividad
+      );
+      if (actividad) {
+        return actividad.nombre;
+      } else {
+        return "Actividad no encontrada";
+      }
+    },
+    async setReservaFinal() {
+      for (let reserva of this.reservas) {
+        this.reservasFinal.push({
+          nombre: this.getNombreActividad(reserva.idActividad),
+          fecha: reserva.fechaHora,
+        });
+      }
+    },
+  },
+};
 </script>
